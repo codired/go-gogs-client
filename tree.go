ddc7eaf2d93ca4d3ac818c19faaa40223b55ed92
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2017-08-13 17:01:20 +0300

    Update get file tree

--- tree.go
+++ tree.go
@@ -1,11 +1,26 @@
 package gogs
 
 import (
-	"github.com/gogits/gogs/models"
 	"fmt"
 )
 
-func (c *Client) GetTree(user, repo, tree string) (*models.TreeFile, error) {
-	files := &models.TreeFile{}
+type TreeFile struct {
+	Name         string `json:"name"`
+	LastCommitId string `json:"commit_id"`
+	IsDir        bool `json:"is_dir"`
+	Size         int64 `json:"size"`
+}
+
+type Tree struct {
+	LastCommitId  string `json:"last_commit_id"`
+	LastCommitMsg string `json:"last_commit_msg"`
+	Files         []TreeFile `json:"files"`
+	Readme        string `json:"readme"`
+	ReadmeName    string `json:"readme_name"`
+	IsReadmeText  bool `json:"is_readme_text"`
+}
+
+func (c *Client) GetTree(user, repo, tree string) (*TreeFile, error) {
+	files := &TreeFile{}
 	return files, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/tree?path=%s", user, repo, tree), nil, nil, files)
 }
